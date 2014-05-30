ac5547a6069c41281d69a95a4bc455d95bef7f5a
kriswu <wuzepingladeng@126.com>
2014-05-31 00:09:19 +0800

    update

--- php_version/ConstentHash.php
+++ php_version/ConstentHash.php
@@ -1,12 +1,56 @@
-<php
+<?php
 
 
-class ConstentHash{
+abstract class flex_hash{
 
-	$vcount ;
+	public abstract function _hash($hashKey);
 
-	$targetToPosition = array();
 }
 
 
+class md5Hash extends flex_hash{
+
+	public function _hash($hashKey){
+
+		return substr(md5($hashKey), 0,8);
+	}
+}
+
+
+class crc32Hash extends flex_hash{
+
+	public function _hash($hashKey){
+		return substr(crc32($hashKey), 0,8)
+	}
+}
+
+class ConstentHash{
+
+
+
+	public function __construct(){
+
+		
+	}
+	$vcount ;
+
+	$targetToPosition = array();
+
+	/**default md5*/
+	public function getHashValueByMethod($method = 'md5',$hashKey){
+
+		if(empty($method)|| empty($hashKey)){
+
+			return false;
+		}
+
+		if($method == 'crc32'){
+
+			return 
+		}
+	}
+
+
+
+}
 ?>
